0ca3aa7bf6532d9d95e26d6c339374e4862954ad
José Domingo Muñoz <josedom24@gmail.com>
2025-11-04 22:24:37 +0100

    Modulo05

--- ejemplos/modulo05/ejercicio5_3.cpp
+++ ejemplos/modulo05/ejercicio5_3.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
 
-struct FuncionBase {
+class FuncionBase {
+public:
+    // Operador virtual: actúa como interfaz polimórfica
     virtual int operator()(int x) const {
         return x;  // identidad
     }
-    virtual ~FuncionBase() = default;
+
+    virtual ~FuncionBase() = default; // Destructor virtual para herencia segura
 };
 
-struct FuncionDoble : FuncionBase {
+// Clase derivada: duplica el valor
+class FuncionDoble : public FuncionBase {
+public:
     int operator()(int x) const override {
         return x * 2;
     }
 };
 
+// Clase derivada: eleva al cuadrado
+class FuncionCuadrado : public FuncionBase {
+public:
+    int operator()(int x) const override {
+        return x * x;
+    }
+};
+
+// Función que usa polimorfismo dinámico
 void ejecutarFuncion(const FuncionBase& f, int valor) {
-    std::cout << f(valor) << "\n";
+    std::cout << "f(" << valor << ") = " << f(valor) << "\n";
 }
 
 int main() {
+    FuncionBase identidad;
     FuncionDoble doble;
-    ejecutarFuncion(doble, 5);  // Imprime 10
+    FuncionCuadrado cuadrado;
+
+    ejecutarFuncion(identidad, 5);  // f(5) = 5
+    ejecutarFuncion(doble, 5);      // f(5) = 10
+    ejecutarFuncion(cuadrado, 5);   // f(5) = 25
 }
