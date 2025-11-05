5af3b46ff968092862a1e212f9feaf5732dd9441
José Domingo Muñoz <josedom24@gmail.com>
2025-11-05 12:12:38 +0100

    Modulo06

--- ejemplos/modulo06/ejercicio3_2.cpp
+++ ejemplos/modulo06/ejercicio3_2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <memory>
 
 // Clase base abstracta
 class Figura {
@@ -22,20 +21,11 @@ Figura crearFiguraPorValor() {
     return c;  // Se produce "object slicing"
 }
 
-// Solución moderna: devolver por puntero inteligente
-std::unique_ptr<Figura> crearFiguraPorPuntero() {
-    return std::make_unique<Circulo>();
-}
-
 int main() {
     std::cout << "== Ejemplo con devolución por valor ==\n";
-    Figura f = crearFiguraPorValor();
+    Figura f = crearFiguraPorValor();  // Se copia solo la parte base
     f.dibujar();  // Error conceptual: no se llama a Circulo::dibujar()
                   // Se comporta como una Figura “recortada”
-
-    std::cout << "\n== Ejemplo con devolución mediante puntero ==\n";
-    auto figuraPtr = crearFiguraPorPuntero();
-    figuraPtr->dibujar();  //  Llama correctamente a Circulo::dibujar()
 
     return 0;
 }
