c144bafa30223bca7b643ce8a5e24bcdcc672a28
José Domingo Muñoz <josedom24@gmail.com>
2025-11-06 17:14:44 +0100

    Módulo04

--- ejemplos/modulo04/ejercicio7_1.cpp
+++ ejemplos/modulo04/ejercicio7_1.cpp
@@ -16,7 +16,7 @@ public:
 };
 
 int main() {
-    Base* b = new Derivada();
-    b->funcion();  // Llama a la versión de Derivada
-    delete b;
+    Derivada d;   // Objeto de tipo Derivada
+    Base& b = d;  // Referencia a Base, enlazada a un objeto Derivada
+    b.funcion();  // Llama a la versión de Derivada gracias al polimorfismo dinámico
 }
