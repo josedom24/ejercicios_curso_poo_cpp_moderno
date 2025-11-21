c7ac6f7607039e18f00c742e9033a01e04ff3454
José Domingo Muñoz <josedom24@gmail.com>
2025-11-21 08:28:14 +0100

    Modulo08

--- ejemplos/modulo08/ejercicio4_1.cpp
+++ ejemplos/modulo08/ejercicio4_1.cpp
@@ -5,15 +5,16 @@
 
 int main() {
     std::vector<int> v = {4, 1, 3, 2};
-    std::array<int, 4> a = {8, 5, 7, 6};
+    std::array<double, 4> a = {8.2, 5.0, 7.4, 6.6};
 
     std::sort(v.begin(), v.end());
     std::sort(a.begin(), a.end());
 
     std::transform(v.begin(), v.end(), v.begin(), [](int x){ return x * 2; });
-    std::transform(a.begin(), a.end(), a.begin(), [](int x){ return x * 2; });
+    std::transform(a.begin(), a.end(), a.begin(), [](double x){ return x * 2; });
 
     for (int x : v) std::cout << x << " ";  // 2 4 6 8
     std::cout << '\n';
-    for (int x : a) std::cout << x << " ";  // 10 12 14 16
+    for (double x : a) std::cout << x << " ";  // 10 13.2 14.8 16.4
+    return 0;
 }
